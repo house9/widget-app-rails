a7f50b602ce1e3a7dbf8ef3ded0828e90eb66026
Jesse House <jesse.house@codingzeal.com>
2023-08-18 10:19:43 -0700

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  resources :widgets
+  resources :widgets do
+    member do
+      post :restock
+    end
+  end
+
+  # mount Sidekiq::Web in your Rails app
+  mount Sidekiq::Web => "/sidekiq"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
