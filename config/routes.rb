c91284986d373628cbd73b017d96dd98ce4e0fa7
Jesse House <jesse.house@codingzeal.com>
2023-08-18 17:29:34 -0700

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
   # mount Sidekiq::Web in your Rails app
   mount Sidekiq::Web => "/sidekiq"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'home', to: 'home#index'
+  root "home#index"
 end
