82574e425d533dffe7a5645912f746e9e50a96b3
Jesse House <jesse.house@codingzeal.com>
2023-08-24 16:40:21 -0700

    add seed data

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
+ruby -v
+echo $RAILS_ENV
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate db:seed
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def create_test_widgets
+  100.times do |i|
+    Widget.create!(
+      name: "Widget #{i}",
+      sku: "SKU-#{i}"
+    )
+  end
+end
+
+def production_seed
+  # no-op
+end
+
+def staging_seed
+  create_test_widgets
+end
+
+def development_seed
+  create_test_widgets
+end
+
+eval("#{Rails.env}_seed")
