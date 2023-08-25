7209a81efa0fdda517a0ea9b10d2b81b2552e909
Jesse House <jesse.house@codingzeal.com>
2023-08-25 14:45:25 -0700

    build client code during deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,20 @@ set -o errexit
 ruby -v
 echo $RAILS_ENV
 
+# rails build
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# client build
+rm -fr client/dist/
+cd client
+npm install
+npm run build
+cd ..
+mkdir -p public/build public/static
+mv client/dist/index.html public/build/index.html
+mv client/dist/static/* public/static
+
+# run database migrations
 bundle exec rake db:migrate
