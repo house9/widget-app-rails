ebd97c14c835a96712feb6737abbeccf34230836
Jesse House <jesse.house@codingzeal.com>
2023-09-06 16:26:59 -0700

    link from client app back to rails app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import "./App.css";
 import "./Widget";
 import Widget from "./Widget";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const railsUrl =
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:3030/";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">React SPA</p>
       <div>
         <Widget />
       </div>
+      <p>
+        <a href={railsUrl}>Back to Rails app</a>
+      </p>
     </>
   );
 }
