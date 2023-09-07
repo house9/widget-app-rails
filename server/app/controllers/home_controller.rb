553447e93cc0bfccc1e1be439c2059317ede0ab9
Jesse House <jesse.house@codingzeal.com>
2023-09-07 12:41:22 -0700

    WIP: random htmx

--- server/app/controllers/home_controller.rb
+++ server/app/controllers/home_controller.rb
@@ -3,6 +3,14 @@ class HomeController < ApplicationController
   end
 
   def hello_htmx
+    # sleep(2)
+
+    # re-write address bar
+    headers['HX-Push'] = Faker::Mountain.name.parameterize
+
+    # actual redirect
+    # headers['HX-Redirect'] = widgets_path
+
     render layout: false
   end
 end
